cd76c0ee4752ac823313fe15aeae8711865fec7e
jasesp5 <jasesp5@gmail.com>
2022-09-29 11:10:48 +0200

    commit inciar

--- Desktop/Dam 2/Acceso Datos/Prueba/src/prueba/Prueba.java
+++ Desktop/Dam 2/Acceso Datos/Prueba/src/prueba/Prueba.java
@@ -17,6 +17,9 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prueba");
+        System.out.println("Prueba");
+        
+        
     }
     
 }
